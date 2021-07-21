8e5e3f249c2403f98fa37e4dbc293bfb8e173934
shine2share <thelam92@gmail.com>
2021-07-21 23:13:10 +0700

    change spring boot version (latest) and add some code to config resource serve

--- taumi-core/src/main/java/com/shine2share/config/OAuth2ResourceServerConfig.java
+++ taumi-core/src/main/java/com/shine2share/config/OAuth2ResourceServerConfig.java
@@ -1,18 +1,29 @@
 package com.shine2share.config;
 
-import com.google.common.collect.ImmutableList;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Value("${jwtPublicKey}")
+    private String publicKey;
+    private final AppAuthenticationEntryPoint appAuthenticationEntryPoint;
+
+    public OAuth2ResourceServerConfig(AppAuthenticationEntryPoint appAuthenticationEntryPoint) {
+        this.appAuthenticationEntryPoint = appAuthenticationEntryPoint;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -25,5 +36,29 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
                 "/swagger-ui.html#!/**",
                 "/v2/**"
         ).permitAll().anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenServices(tokenServices());
+        resources.authenticationEntryPoint(appAuthenticationEntryPoint);
+    }
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(tokenConverter());
+    }
+    @Bean
+    public JwtAccessTokenConverter tokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setVerifierKey(publicKey);
+        jwtAccessTokenConverter.setSigningKey("mykey");
+        return jwtAccessTokenConverter;
+    }
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
     }
 }
